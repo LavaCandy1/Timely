4dd9d9b70e42829bd2bde6e94e7e6252686e16cb
LavaCandy1 <aayush.garg.1793@gmail.com>
2025-09-22 10:12:14 +0530

    Starting Timely

---
+++ src/test/java/com/example/Timely/TimelyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Timely;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimelyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
