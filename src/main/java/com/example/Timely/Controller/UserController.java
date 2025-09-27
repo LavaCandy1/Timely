a60cf13bb5630604ee96893e29dc73e1d303dfb2
LavaCandy1 <aayush.garg.1793@gmail.com>
2025-09-27 20:49:05 +0530

    get all users endpoint added

--- src/main/java/com/example/Timely/Controller/UserController.java
+++ src/main/java/com/example/Timely/Controller/UserController.java
@@ -1,12 +1,29 @@
 package com.example.Timely.Controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.Timely.Models.dto.UserResponseDTO;
+import com.example.Timely.Service.UserService;
+
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/users")
 public class UserController {
 
-    
+    @Autowired
+    UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<UserResponseDTO>> getUsers() {
+
+        return ResponseEntity.ok(userService.getAllUsers());
+
+        
+    }
     
 }
