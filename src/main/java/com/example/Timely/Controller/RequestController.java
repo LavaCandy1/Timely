da9943163b7876d4378b859fb28f979bcfa16781
LavaCandy1 <aayush.garg.1793@gmail.com>
2026-03-12 23:48:39 +0530

    Added intructor in requestData from securityContext

--- src/main/java/com/example/Timely/Controller/RequestController.java
+++ src/main/java/com/example/Timely/Controller/RequestController.java
@@ -1,6 +1,8 @@
 package com.example.Timely.Controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,7 +18,9 @@ public class RequestController {
     @PostMapping("/reschedule")
     public ResponseEntity<Void> createRescheduleRequest(@RequestBody RescheduleRequest incomingRequestData) {
         // Logic to create a reschedule request
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         System.out.println("Creating a reschedule request...");
+        incomingRequestData.setInstructor(auth.getName());
         System.out.println(incomingRequestData);
         return ResponseEntity.ok().build();
     }
