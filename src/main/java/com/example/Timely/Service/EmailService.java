7b58ea36c739e80b7ac12f246476fb2a87e380c4
LavaCandy1 <aayush.garg.1793@gmail.com>
2026-03-24 15:58:46 +0530

    added mass mail logic ~ did not use it yet

--- src/main/java/com/example/Timely/Service/EmailService.java
+++ src/main/java/com/example/Timely/Service/EmailService.java
@@ -1,5 +1,7 @@
 package com.example.Timely.Service;
 
+import java.util.List;
+
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
@@ -26,6 +28,25 @@ public class EmailService {
             System.out.println("Email sent to: " + to);
         } catch (Exception e) {
             System.out.println("Failed to send email: " + e.getMessage());
+        }
+    }
+
+    @Async
+    public void sendMassEmail(List<String> recipients, String subject, String body
+        // ,String instructor
+        ) {
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            String[] to = recipients.toArray(new String[0]);
+            message.setTo(to);
+            // message.setBcc(instructor); // BCC the instructor to keep them informed
+            message.setSubject(subject);
+            message.setText(body);
+            
+            mailSender.send(message);
+            System.out.println("Mass email sent to: " + String.join(", ", to));
+        } catch (Exception e) {
+            System.out.println("Failed to send mass email: " + e.getMessage());
         }
     }
 
